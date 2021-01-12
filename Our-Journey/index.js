0ee85725ef216bd7f4cec90f25b1bfd10786072a
Karan01406 <67190619+Karan01406@users.noreply.github.com>
2021-01-12 11:32:30 +0530

    Update index.js

--- Our-Journey/index.js
+++ Our-Journey/index.js
@@ -23,5 +23,9 @@ function createChat() {
       answer =
         "https://www.canva.com/design/DAESeAPgcvY/7b9Vy0Ww16lx-BAR0YDsng/watch?utm_content=DAESeAPgcvY&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink";
     }
+      
+      else if(question === "Bhavya23"){
+      answer = "https://www.canva.com/design/DAESiBBehBc/QLww56t4rVL4EbJR3_5G8A/watch?utm_content=DAESiBBehBc&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink";
+      }
     return answer;
 }
